b6366ae58d81bd2400c5e7ae4e1ed2cddd495757
peter jose de sousa <peter.josedesousa@smoothwall.net>
2016-12-05 14:39:27 +0000

    Basic biew returns minimal HTML

--- src/functional_tests.py
+++ src/functional_tests.py
@@ -8,21 +8,21 @@ assert 'To-Do' in browser.title
 
 browser.quit()
 
-class NewVisitorTEst(unittest.TestCase):
+class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start__a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         #editth has heard about a cool new online t-do app She Goes
         # to check out it's homepage
         self.browser.get('http://localhost:8000')
 
         # She notices the page tutle and heard ention to-do lists
-        self.assertIn('To-Do', self.brwoser.title)
-        self.fail('Finish the test!')
+        self.assertIn('To-Do', self.browser.title)
+        #self.fail('Finish the test!')
 
         #She is invited to enter a to-do item straight away
 
