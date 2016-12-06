bebe2020ccab9185327dd69b7f3791cd5af93de6
peter jose de sousa <peter.josedesousa@smoothwall.net>
2016-12-06 14:13:27 +0000

    Current progress with Chp 5

--- src/functional_tests.py
+++ src/functional_tests.py
@@ -17,7 +17,7 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        #editth has heard about a cool new online t-do app She Goes
+        # editth has heard about a cool new online t-do app She Goes
         # to check out it's homepage
         self.browser.get('http://localhost:8000')
 
@@ -26,28 +26,28 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        #She is inbited to enter a to-do item straight away
+        # She is inbited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
                 inputbox.get_attribute('placeholder'),
                 'Enter a to-do item'
         )
 
-        #She types "Buy peacock feathers" into a testbox (Edith's Hobby
+        # She types "Buy peacock feathers" into a testbox (Edith's Hobby
         # is tying fly-fishing lures
         inputbox.send_keys('Buy peacock feathers')
 
-        #When she hits enter, the page updates, and now the page lists
+        # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
         )
-
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edit is very methodical)
         self.fail('Finish the test!')
